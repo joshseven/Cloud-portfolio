28d874f41b838778ea30b0d6ca1bfa4309fff859
joshseven <neves.joshua@gmail.com>
2019-10-29 20:34:20 -0400

    added script to AWS Lambda

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,20 +1,28 @@
+import json
 import boto3
 import io
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3')
 
+def lambda_handler(event, context):
 
-portfolio_bucket = s3.Bucket('portfolio.joshneves.info')
-build_bucket = s3.Bucket('portfoliobuild.joshneves.info')
+    s3 = boto3.resource('s3')
 
-portfolio_zip = io.BytesIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    portfolio_bucket = s3.Bucket('portfolio.joshneves.info')
+    build_bucket = s3.Bucket('portfoliobuild.joshneves.info')
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(
-            obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    portfolio_zip = io.BytesIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(
+                obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
